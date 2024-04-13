f4aad4147523ced79fda3f33dae37a63a3d4b9c4
Sultonov557 <sultonovzerifboy@gmail.com>
2024-04-13 21:06:25 +0500

    .

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const time = document.querySelector("#time");
 
+setWeather();
+
+function setWeather() {
+    
+}
+
 setInterval(() => {
 	let timeString = new Date().toLocaleTimeString(undefined, { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
 	let dateString = new Date().toLocaleDateString(undefined, { day: "2-digit", month: "2-digit", year: "2-digit", weekday: "short" });
 
-	time.innerHTML = 
-`${timeString}
+	time.innerHTML = `${timeString}
 ${dateString}`;
-}, 100);
+}, 500);
