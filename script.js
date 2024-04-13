1a93d2888a6048a2dbbdb0525d444fa47ad4f0e4
Sultonov557 <sultonovzerifboy@gmail.com>
2024-04-13 20:15:38 +0500

    .

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function addCard(){
-    
-}
+const time = document.querySelector("#time");
+
+setInterval(() => {
+	let timeString = new Date().toLocaleTimeString(undefined, { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
+	let dateString = new Date().toLocaleDateString(undefined, { day: "2-digit", month: "2-digit", year: "2-digit", weekday: "short" });
+
+	time.innerHTML = 
+`${timeString}
+${dateString}`;
+}, 100);
